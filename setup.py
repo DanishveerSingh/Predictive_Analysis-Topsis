67c0607373efb8bf446d149005013a3228c9f22d
Danishveer Singh <danishveersingh2004@gmail.com>
2026-02-12 21:43:55 +0530

    add "topsis"

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name = 'Topsis-DanishveerSingh-102317081', 
+    version = '0.1',
+    author = "Danishveer Singh",
+    author_email = "dsingh3_be23@thapar.edu",
+    description = "A Topsis Report Generator",
+    packages = setuptools.find_packages(),
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+)
+ 
